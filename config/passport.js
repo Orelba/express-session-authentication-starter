a2e2d1dcd8d44a0a415f1c843c16cab03167bed1
Orelba <88292816+Orelba@users.noreply.github.com>
2024-06-27 00:20:33 +0300

    Finish lesson

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,45 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
-const User = connection.models.User;
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const connection = require('./database')
+const User = connection.models.User
+const validPassword = require('../lib/passwordUtils').validPassword
 
-// TODO: passport.use();
+const customFields = {
+  usernameField: 'uname',
+  passwordField: 'pw',
+}
+
+const verifyCallback = async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username: username })
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username' })
+    }
+
+    const isValid = validPassword(password, user.hash, user.salt)
+    if (!isValid) {
+      return done(null, false, { message: 'Password incorrect' })
+    }
+
+    return done(null, user)
+  } catch (err) {
+    return done(err)
+  }
+}
+
+const strategy = new LocalStrategy(customFields, verifyCallback)
+
+passport.use(strategy)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (userId, done) => {
+  try {
+    const user = await User.findById(userId)
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
